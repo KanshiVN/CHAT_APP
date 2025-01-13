064ecd93ff0091e9340e9589abd10506d1239eab
kanshi <kanshinale332@gmal.com>
2025-01-13 17:01:40 +0530

    Added message Routes and Controller [most of the backend part is done, For now switching to the frontend ]

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
@@ -46,10 +47,67 @@ export const signup = async (req,res)=>{
         }
 }
 
-export const login = (req,res)=>{
+export const login = async (req,res)=>{
+    const{email, password} = req.body;
+    try{
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(404).json({message:"Invalid credentials"});
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if(!isPasswordCorrect){
+            return res.status(404).json({message:"Invalid credentials"});
+        }
+        generateToken(user._id,res);
+        res.status(200).json({_id:user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic});
+        } 
+        catch(error){
+            console.error('Error in login controller', error);
+            res.status(500).json({message:'Server error'});
+
+        }
 
 }
 
 export const logout = (req,res)=>{
-   
+   try{
+    res.cookie("jwt","",{maxAge:0});
+    res.status(200).json({message:'Successfully logged out'});
+   }
+   catch(error){
+       console.error('Error in logout controller', error);
+       res.status(500).json({message:'Server error'});
+   }
+}
+
+export const updateProfile = async (req,res)=>{
+    try{
+        const{profilePic} = req.body;
+        const userId = req.user._id;
+
+        if(!profilePic){
+            return res.status(400).json({message:'Please provide profile picture.'});
+        }
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new:true});
+
+        res.status(200).json(updatedUser);
+    }
+    catch(error){
+        console.error('Error in updateProfile controller', error);
+        res.status(500).json({message:'Server error'});
+    }
+}
+
+export const checkAuth = (req, res) => {
+    try{
+        res.status(200).json(req.user)
+    }
+    catch(error){
+        console.error('Error in checkAuth controller', error);
+        res.status(500).json({message:'Server error'});
+    }
 }
---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,66 @@
+import User from "../models/user.model";
+import Message from "../models/message.model.js";
+    export const getUserForSidebar = async ()=>{
+        try{
+            const loggedInUserId = req.user._id;
+            const filterUser = await User.findById({_id:{$ne:loggedInUserId}}).select('-password');
+            //we find all the users other than the current user {LIke the friends of the user}
+            res.status(200).json(filterUser);
+        }
+        catch(err){
+            res.status(500).json({message:err.message});
+            console.log("Error in getUserForSidebar:",err.message);
+
+        }
+        
+    }
+
+    export const getMessage = async (req, res) =>{
+        try {
+            const {id:userToChatId }= req.params; // receiver id
+            const senderId = req.user_id; // my id
+            const messages = await Message.find({
+                $or:[
+                    {senderId:senderId, receiverId:userToChatId},
+                    {senderId:userToChatId, receiverId:senderId}
+                ] // this is the array of messages
+            })
+            res.status(200).json(messages);
+        } 
+        catch (error) {
+            console.error("Error in getMessage:", error.message);
+            res.status(500).json({ message: "Server Error" });
+            
+        }
+    }
+
+    export const sendMessage = async (req,res) =>{
+        try {
+            const {text, image} = req.body;
+            const{id:receiverId}= req.params;
+            const senderId = req.user_id;
+
+            let imageUrl;
+            if(image){
+                // If the image is provided by the user then we upload it to the cloudinary
+                const uploadResponse = await cloudinary.uploader.upload(image);
+                imageUrl = uploadResponse.secure_url;
+
+                const newMessage = new Message({
+                    senderId,
+                    receiverId, 
+                    text,
+                    image:imageUrl
+                });
+                await newMessage.save();
+
+                // Realtime functionality goes here
+                res.status(201).json({newMessage})
+            }
+            
+        } catch (error) {
+            console.error("Error in sendMessage controller:", error.message);
+            res.status(500).json({ message: "Server Error" });
+            
+        }
+    };
